49fc3ffccd5a363503f2a0c691c7bf72b7b2568e
Georgy Pogorelov <zhpogorelov@yandex.ru>
2021-10-28 20:33:44 +0300

    add cart

--- routes/viewpost.router.js
+++ routes/viewpost.router.js
@@ -1,18 +1,26 @@
 const router = require('express').Router();
-const { Post } = require('../db/models');
+const { Post, User } = require('../db/models');
 
 router
   .route('/:postId')
   .get(async (req, res) => {
     let post;
+    let isSame;
     try {
-      post = await Post.findOne({ where: { id: req.params.postId }, raw: true });
+      post = await Post.findOne({ where: { id: req.params.postId }, raw: true, include: User });
     } catch (error) {
       return res.render('error', {
         message: 'Ошибки при получении данных карточки',
         error: {},
       });
     }
+    if (req.session.user?.id === post.UserId) {
+      isSame = true;
+      return res.render('viewPost', {
+        post, isSame, user: req.session.user,
+      });
+    }
+    isSame = false;
     return res.render('viewPost', { post, user: req.session.user });
   });
 
