6b58392d46595d8217e838d747cc42a2d98b7ade
Sergio <dsbox@list.ru>
2021-10-28 14:10:51 +0300

    Added: index router. Temp changes: card css, index and layout hbs

--- routes/index.router.js
+++ routes/index.router.js
@@ -1,7 +1,11 @@
 const router = require('express').Router();
+const { Post } = require('../db/models');
 
-router.get('/', (req, res) => {
-  res.render('index');
+router.get('/', async (req, res) => {
+  const posts = await Post.findAll({ raw: true });
+  console.log(posts);
+
+  res.render('index', { posts });
 });
 
 module.exports = router;
