74b53939ce898f0d7d1dcca8ac7ea9fb41eaeced
Georgy Pogorelov <zhpogorelov@yandex.ru>
2021-10-29 14:38:45 +0300

    some fix

--- routes/deleteCard.router.js
+++ routes/deleteCard.router.js
@@ -2,11 +2,11 @@ const router = require('express').Router();
 const { Post } = require('../db/models');
 
 router
-  .delete('/:id',async (req,res) => {
+  .delete('/:id', async (req, res) => {
     console.log(req.params.id);
-    const cardId = await Post.findOne({where: {id: req.params.id}})
-    await cardId.destroy({where: {id: req.params.id}});
-    return res.json({ok: '/profile'});
+    const cardId = await Post.findOne({ where: { id: req.params.id } });
+    await cardId.destroy({ where: { id: req.params.id } });
+    return res.json({ ok: '/' });
   });
 
 module.exports = router;
